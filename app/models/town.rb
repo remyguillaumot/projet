41c0fb6f6a2ff345cc75c3fff1e30ae05f22f51b
guillaumotremy <guillaumotremy@gmail.com>
2018-03-17 06:33:40 -0400

    Add postcode

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,12 +1,13 @@
 class Town < ActiveRecord::Base
-  validates :name, :latitude, :longitude, presence: true
+  validates :name, :postCode, :latitude, :longitude, presence: true
   before_validation :geocode
   
   private
   def geocode
-    towns = Nominatim.search.city(self.name).limit(1)
+    towns = Nominatim.search.city(self.name).limit(1).address_details(true)
     if towns && towns.first
       current_town = towns.first
+      self.postCode = current_town.address.postcode
       self.latitude = current_town.lat
       self.longitude = current_town.lon
     end
