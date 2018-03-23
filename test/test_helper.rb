5db36891b83221cf4186ddd6cc8329026ea4847b
guillaumotremy <guillaumotremy@gmail.com>
2018-03-23 13:18:03 -0400

    test coverage conf

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,5 @@
-if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
   SimpleCov.start 'rails'
-  puts "required simplecov"
-end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
